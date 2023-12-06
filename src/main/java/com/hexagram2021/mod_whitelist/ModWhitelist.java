0d13883055acfcbcb0d982140413099c682188e9
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-12-06 20:18:22 +0800

    first commit

---
+++ src/main/java/com/hexagram2021/mod_whitelist/ModWhitelist.java
@@ -0,0 +1,10 @@
+package com.hexagram2021.mod_whitelist;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class ModWhitelist implements DedicatedServerModInitializer {
+	@Override
+	public void onInitializeServer() {
+
+	}
+}
---
+++ src/main/java/com/hexagram2021/mod_whitelist/client/ModWhitelistClient.java
@@ -0,0 +1,10 @@
+package com.hexagram2021.mod_whitelist.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModWhitelistClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
