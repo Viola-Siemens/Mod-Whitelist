237e7e7e7bcb7e3eb7f83d7f0793d14f08797d7b
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-12-07 18:44:50 +0800

    run文件夹结构化，以及一句没啥用的说明

--- src/main/java/com/hexagram2021/mod_whitelist/server/config/MWServerConfig.java
+++ src/main/java/com/hexagram2021/mod_whitelist/server/config/MWServerConfig.java
@@ -216,6 +216,7 @@ public class MWServerConfig {
 			writer.write("# Adding a mod to whitelist and blacklist\n\n");
 			writer.write("First, you should find the identifier of the mod (modid), a simple way is open the jar file with an archiver software (eg. WinZip, HaoZip, 7-Zip), open \"fabric.mod.json\" and see what the value of key \"id\" is. For example, the modid of Mod Whitelist mod is \"mod_whitelist\".\n\n");
 			writer.write("Then, add it to `CLIENT_MOD_NECESSARY` field if you want client players install it. By default, it is blacklist mode, so you can add it to `CLIENT_MOD_BLACKLIST` field if you do not want client players install it. If you want to use whitelist mode instead, set `USE_WHITELIST_ONLY` to true and add all whitelist modids to `CLIENT_MOD_WHITELIST` field.\n\n");
+			writer.write("In addition, if `USE_WHITELIST_ONLY` is true, `CLIENT_MOD_BLACKLIST` field is just ignored while running the server. And if `USE_WHITELIST_ONLY` is true, `CLIENT_MOD_WHITELIST` field is ignored instead.\n\n");
 
 			writer.write("# Issue tracker\n\n");
 			writer.write("Visit https://github.com/Viola-Siemens/Mod-Whitelist/issues and post your issue and logs if you find any problems with this mod.\n");
