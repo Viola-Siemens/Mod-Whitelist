8d1e360027cd8632a862ab1fc3a119d02a24eb7d
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-12-11 21:30:09 +0800

    用户文档内容增加

--- src/main/java/com/hexagram2021/mod_whitelist/server/config/MWServerConfig.java
+++ src/main/java/com/hexagram2021/mod_whitelist/server/config/MWServerConfig.java
@@ -268,11 +268,12 @@ public class MWServerConfig {
 			writer.write("- Installed on the server side but not installed on the client side. The client player is not allowed to enter the server and sent message \"multiplayer.disconnect.mod_whitelist.packet_corruption\".\n\n");
 
 			writer.write("# Adding a mod to whitelist and blacklist\n\n");
+			writer.write("The config file is in \"&lt;server directory&gt;/config/mod_whitelist-config.json\". If you want to add mods to the whitelist or blacklist, please read the following guides.\n\n");
 			writer.write("First, you should find the identifier of the mod (modid), a simple way is open the jar file with an archiver software (eg. WinZip, HaoZip, 7-Zip), open \"fabric.mod.json\" and see what the value of key \"id\" is. For example, the modid of Mod Whitelist mod is \"mod_whitelist\".\n\n");
 			writer.write("Then, add it to `CLIENT_MOD_NECESSARY` field if you want client players install it. By default, it is blacklist mode, so you can add it to `CLIENT_MOD_BLACKLIST` field if you do not want client players install it. If you want to use whitelist mode instead, set `USE_WHITELIST_ONLY` to true and add all whitelist modids to `CLIENT_MOD_WHITELIST` field.\n\n");
 			writer.write("In addition, if `USE_WHITELIST_ONLY` is true, `CLIENT_MOD_BLACKLIST` field is just ignored while running the server. And if `USE_WHITELIST_ONLY` is true, `CLIENT_MOD_WHITELIST` field is ignored instead.\n\n");
 			writer.write("As you might see, if fabric-api is installed, the modlist will contains quite a lot of modids. You can run a client with this mod installed, and open \".minecraft/logs/latest.log\", and you will see the following format line to simplify gathering the modlist manually:\n\n");
-			writer.write("```\n\nMod Whitelist vx.x.x from the client! Modlist: [\"fabric-api\", \"fabric-api-base\", ...]\n\n```\n\n");
+			writer.write("```\nMod Whitelist vx.x.x from the client! Modlist: [\"fabric-api\", \"fabric-api-base\", ...]\n```\n\n");
 
 			writer.write("# Issue tracker\n\n");
 			writer.write("Visit https://github.com/Viola-Siemens/Mod-Whitelist/issues and post your issue and logs if you find any problems with this mod.\n");
